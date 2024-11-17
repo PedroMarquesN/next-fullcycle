32a8e7ca084e28e5c824218ca691517fda43439d
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-11-17 01:16:04 -0300

    Feat: add header components and implement scroll detection for dynamic styling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,52 +1,22 @@
 'use client';
-import { useEffect, useState } from "react";
+import { UserProfile } from "./UserProfile";
+import { NavLinks } from "./NavLinks";
+import { Logo } from "./Logo";
+import { useScroll } from "../hooks/useScroll";
 
 export default function Header() {
-    const [isScrolled, setIsScrolled] = useState(false);
-
-    useEffect (() => {
-        const handleScroll = () => {
-            if (window.scrollY > 0) {
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
-            }
-        }
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
+    const isScrolled = useScroll();
+    
   return (
     <header className={`${isScrolled && 'bg-black'}  fixed top-0 z-50 flex w-full items-center  transition-all justify-between px-4 py-4 lg:px-10 lg:py-4`}>
       <div className='flex items-center lg:space-x-5'>
-        <img
-          src='https://rb.gy/ulxxee'
-          alt='netflix'
-          width={120}
-          height={120}
-        />
+        <Logo />
+        <NavLinks />
 
-        <ul className='hidden space-x-4  md:flex'>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Latest</li>
-          <li>My List</li>
-        </ul>
       </div>
 
-      <div className='flex items-center space-x-4'>
-        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
-        <img 
-            src='https://rb.gy/g1pwyx'
-            alt='avatar'
-            className='rounded cursor-pointer'
-        />
-      </div>
+        <UserProfile />
+
     </header>
   );
 }
---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,13 @@
+'use client';
+import Image from "next/image";
+
+export const Logo = () => {
+    return (
+        <Image
+            src='/logo.svg'
+            alt='netflix'
+            width={120}
+            height={120}
+            className="cursor-pointer" />
+    );
+};
