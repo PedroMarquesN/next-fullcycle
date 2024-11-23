20778ced01a6194eb17c360e4a636c5602f067a4
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-11-23 18:53:57 -0300

    Feat: refactor authentication forms to use a reusable AuthForm component for login and registration

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -1,6 +1,5 @@
 'use client';
-import Link from "next/link";
-import { InputField } from "../InputField";
+import { AuthForm } from "@/app/components/AuthForm";
 
 export default function LoginForm() {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -9,36 +8,8 @@ export default function LoginForm() {
         }
     
 
-    return <form onSubmit={handleSubmit} 
-    className='flex w-full max-w-96 flex-col space-y-4 rounded bg-[#141414] bg-opacity-90 px-4 py-8 shadow-lg'>
-      <div className='flex flex-col items-center space-y-4'>
-        <h1 className='text-3xl font-bold'>Login</h1>
-        <p className='text-sm text-gray-400'>
-          Novo por aqui? {''}
-          <Link href='/auth/register' className='text-blue-500 hover:underline'>Crie uma conta</Link>
-        </p>
-      </div>
-      <div className='flex flex-col space-y-4'>
-        <InputField
-          id='email'
-          name='email'
-          label='Email'
-          type='email'
-          placeholder='Digite seu Email' />
-        <InputField
-          id='password'
-          name='password'
-          label='Senha'
-          type='password'
-          placeholder='Digite sua Senha' />
-      </div>
-      <div className='flex flex-col-reverse space-y-2 pt-2 sm:flex-row sm:space-x-2 sm:space-y-0'>
-        <button
-          className='rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 sm:w-auto sm:px-8'
-          type='submit'
-        >
-          Entrar
-        </button>
-      </div>
-    </form>;
+    return (
+      <AuthForm formType='login' onSubmit={handleSubmit} />
+    )
+
   }
--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -1,15 +1,67 @@
-import { FC } from "react";
+import Link from 'next/link';
+import { FC } from 'react';
+import { InputField } from './InputField';
 
 type AuthFormProps = {
-    formType: 'login' | 'register';
-    onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-}
+  formType: 'login' | 'register';
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+};
 
+export const AuthForm: FC<AuthFormProps> = ({ formType, onSubmit }) => {
+  return (
+    <form
+      onSubmit={onSubmit}
+      className='flex w-full max-w-96 flex-col space-y-4 rounded bg-[#141414] bg-opacity-90 px-4 py-8 shadow-lg'
+    >
+      <div className='flex flex-col items-center space-x-4'>
+        <h1 className='text-3xl font-bold'>
+          {formType === 'login' ? 'Login' : 'Register'}
+        </h1>
+        <p className='text-sm text-gray-400'>
+          {formType === 'login' ? 'Novo por aqui?' : 'Já tem uma conta?'}
+          {' '}
+          <Link
+            href={formType === 'login' ? '/auth/register' : '/auth/login'}
+            className='text-blue-500 hover:underline'
+          >
+            {formType === 'login' ? 'Crie uma conta' : 'Faça login'}
+          </Link>
+        </p>
+      </div>
+      <div className='flex flex-col space-y-4'>
+        <InputField
+          id='email'
+          name='email'
+          label='Email'
+          type='email'
+          placeholder='Digite seu Email'
+        />
+        <InputField
+          id='password'
+          name='password'
+          label='Senha'
+          type='password'
+          placeholder='Digite sua Senha'
+        />
+        {formType === 'register' && (
+          <InputField
+            id='confirmPassword'
+            name='confirmPassword'
+            label='Confirme sua Senha'
+            type='password'
+            placeholder='Confirme sua Senha'
+          />
+        )}
+      </div>
+      <div className='flex flex-col-reverse space-y-2 pt-2 sm:flex-row sm:space-x-2 sm:space-y-0'>
+        <button
+          type='submit'
+          className='w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+        >
+          {formType === 'login' ? 'Login' : 'Registrar'}
+        </button>
 
-export const AuthForm: FC<AuthFormProps> = () => {
-    return (
-        <div>
-            <h1>AuthForm</h1>
-        </div>
-    );
-}
+      </div>
+    </form>
+  );
+};
