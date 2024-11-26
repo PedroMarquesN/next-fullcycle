0e560a70581802671fc696b10e9e1d6842102ae3
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-11-26 18:02:02 -0300

    Feat: enhance MovieRow component to display dynamic movie data by genre; refactor MovieCard for improved layout and functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
 import Header from './components/Header';
 import { MovieRow } from './components/MovieRow';
 import { Banner } from './components/Banner';
-import { getFeaturedMovie } from './service/MovieService';
+import { getFeaturedMovie, getMoviesByGenre } from './service/MovieService';
 
 export default async function Home() {
-  const featuredMovie = await getFeaturedMovie('101');
+  const featuredMovie = await getFeaturedMovie('106');
+  const genres = [
+    'Drama',
+    'Action',
+    'Comedy',
+    'Animation',
+  ];
+  const movies = await Promise.all(
+    genres.map(async (genre) =>{
+      const movies = await getMoviesByGenre(genre, { _limit: 8 });
+      return {sectionTitle: genre, movies };
+    })
+  );
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-full'>
       <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
-        <Banner movie={featuredMovie}/>
+        <Banner movie={featuredMovie} />
+      {movies.map((movie) => (
+        <MovieRow key={movie.sectionTitle} sectionTitle={movie.sectionTitle} movies={movie.movies} />
+      ))}
 
-        <MovieRow sectionTitle='Featured' />
-        <MovieRow sectionTitle='Popular' />
-        <MovieRow sectionTitle='Top Rated' />
       </main>
     </div>
   );
